a9dcc30430c75ca4810019df36d35d5cb97a8d73
Carlos <61396980+gerardoc96@users.noreply.github.com>
2024-12-17 16:16:29 -0800

    Removed irrelevant code, added four Pokémon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
-alert('Hello world');
-let favoriteColor = 'Cyan';
-document.write(favoriteColor);
+
+let squirtle = {
+  name: 'Squirtle',
+  height: 0.5,
+  evolution: wartortle,
+  moves: ['Tail whip', 'Tackle'],
+};
+
+let wartortle = {
+  name: 'Wartortle',
+  height: 1,
+  evolution: blastoise,
+  moves: ['Bubble', 'Water Gun'],
+};
+
+let vulpix = {
+  name: 'Vulpix',
+  height: 0.6,
+  evolution: ninetales,
+  moves: ['Tail whip', 'Ember'],
+};
+
+let ninetales = {
+  name: 'Ninetales',
+  height: 1.1,
+  evolution: null,
+  moves: ['Nasty plot', 'Safeguard'],
+};
+
+let pokemonList = [
+  squirtle,
+  wartortle,
+  vulpix,
+  ninetales,
+];
+
