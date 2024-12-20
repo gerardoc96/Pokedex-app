53aec38e2bd5813f3d84754a7dc8749c3029110c
Carlos <61396980+gerardoc96@users.noreply.github.com>
2024-12-19 18:12:18 -0800

    Added for loop to display the list of Pokémon.

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,35 @@ const pokemonList = [
   ninetales,
 ];
 
+
+const pokemonListUl = document.getElementById("pokemon-list");
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let listItem = document.createElement("li");
+
+  // Base text for pokemon's name
+  listItem.classList.add("pokemon-list__item")
+
+  //elements for the name and height
+  let pokemonName = document.createElement("div");
+  pokemonName.textContent = pokemonList[i].name;
+  pokemonName.classList.add("pokemon-list__name")
+
+  let pokemonHeight = document.createElement("div");
+  pokemonHeight.textContent = `Height: ${pokemonList[i].height}m`;
+  pokemonHeight.classList.add("pokemon-list__height")
+
+  listItem.appendChild(pokemonName);
+  listItem.appendChild(pokemonHeight);
+
+  //highlights pokemon taller than 1m
+  if (pokemonList[i].height > 1) {
+    let bigMessage = document.createElement("div");
+    bigMessage.textContent = "Wow, that's big!"
+    bigMessage.classList.add("pokemon-list__big");
+    listItem.appendChild(bigMessage);
+  }
+
+  pokemonListUl.appendChild(listItem);
+}
+
