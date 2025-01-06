d804075c29e00ba9e69dca7de51ccc45b28a7262
Carlos <61396980+gerardoc96@users.noreply.github.com>
2025-01-05 16:02:30 -0800

    Refactored for improved readability and maintainability.

--- js/pokemonRepository.js
+++ js/pokemonRepository.js
@@ -8,64 +8,73 @@ const pokemonRepository = (function () {
   function add(pokemon) {
 
     if (typeof pokemon === 'object' && 'name' in pokemon && 'detailsUrl' in pokemon) {
-
       pokemonList.push(pokemon);
 
     } else {
-
       console.error('Invalid Pokémon object. Ensure it has the right properties.');
-
     }
-  }
-
-  function loadList() {
-    pokemonUI.showLoadingMessage();
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-
-    }).then(function (json) {
-      pokemonUI.hideLoadingMessage();
-      json.results.forEach(function (item) {
-
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-      });
-
-    }).catch(function (e) {
-      pokemonUI.hideLoadingMessage();
-      console.error(e);
-    });
-
   }
 
   function loadDetails(pokemon) {
     pokemonUI.showLoadingMessage();
     return fetch(pokemon.detailsUrl)
-
-      .then(function (response) {
-        return response.json();
-
-      }).then(function (details) {
-        pokemonUI.hideLoadingMessage();
-        pokemon.imgUrl = details.sprites.front_default;
-        pokemon.height = details.height;
-        pokemon.type = details.types.map(function (typeInfo) {
-          return typeInfo.type.name;
-        });
-
-      }).catch(function (e) {
-        pokemonUI.hideLoadingMessage();
-        console.error(e);
-      });
-
-
+      .then(fetchPokemonDetails)
+      .then(function (details) {
+        processPokemonDetails(details, pokemon);
+      })
+      .catch(handleLoadListError);
   }
+
+  function loadList() {
+    pokemonUI.showLoadingMessage();
+    return fetch(apiUrl)
+      .then(fetchPokemonList)
+      .then(processPokemonList)
+      .catch(handleLoadListError)
+  }
+
   function getAll() {
     return pokemonList;
   }
+
+  function getPokemonType(typeInfo) {
+    return typeInfo.type.name;
+  }
+
+  function fetchPokemonList(response) {
+    return response.json();
+  }
+
+  function fetchPokemonDetails(response) {
+    return response.json();
+  }
+
+  function processPokemonDetails(details, pokemon) {
+    pokemonUI.hideLoadingMessage();
+    pokemon.imgUrl = details.sprites.front_default;
+    pokemon.height = details.height;
+    pokemon.type = details.types.map(getPokemonType);
+  }
+
+  function processPokemonList(json) {
+    pokemonUI.hideLoadingMessage();
+    json.results.forEach(addPokemonFromList);
+  }
+
+
+  function addPokemonFromList(item) {
+    let pokemon = {
+      name: item.name,
+      detailsUrl: item.url
+    };
+    add(pokemon);
+  }
+
+  function handleLoadListError(e) {
+    pokemonUI.hideLoadingMessage();
+    console.error(e);
+  }
+
 
   return {
     add,
