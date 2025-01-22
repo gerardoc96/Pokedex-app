2d1f175d501cf7c943446e93a8f232b1565bf35b
Carlos <61396980+gerardoc96@users.noreply.github.com>
2025-01-21 17:24:03 -0800

    Added a navigation bar, with search functionality

--- js/app.js
+++ js/app.js
@@ -6,6 +6,30 @@ document.addEventListener('DOMContentLoaded', function () {
     let allPokemon = pokemonRepository.getAll();
     pokemonUI.populatePokemonList(allPokemon, pokemonListUI);
 
+    //Search functionality
+    let searchform = document.querySelector('form[role="search"]');
+    searchform.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      // Making it case-insensitive
+      let searchInput = searchform.querySelector('input[type="search"]').value.toLowerCase();
+      let filteredPokemon = allPokemon.filter(function (pokemon) {
+        return pokemon.name.toLowerCase().includes(searchInput);
+      });
+      pokemonUI.populatePokemonList(filteredPokemon, pokemonListUI);
+    });
+
+    // Live search functionality
+    let searchInputLive = searchform.querySelector('input[type="search"]');
+    searchInputLive.addEventListener('input', function () {
+      let searchInput = searchInputLive.value.toLowerCase();
+      let filteredPokemon = allPokemon.filter(function (pokemon) {
+        return pokemon.name.toLowerCase().includes(searchInput);
+      });
+      pokemonUI.populatePokemonList(filteredPokemon, pokemonListUI);
+    });
+
+
   }).catch(function (error) {
     console.error('Error loading Pokemon:', error);
   });
